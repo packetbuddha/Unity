85008eeeadce24a9c8525d780d2f86662b4b0b7e
Carl Tewksbury <packetbuddha@gmail.com>
2016-12-11 13:06:46 -0800

    with mouse position

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour
+{
+    float x;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.mousePosition.x / Screen.width * 16;
+        if (x <= 1 | x >= 15) {
+            print("Hey, you're going out of bounds!");
+        }
+    }
+}
