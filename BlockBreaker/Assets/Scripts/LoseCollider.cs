a9f36a48e9116b4f8943a81d1d59f9e024bed080
Carl Tewksbury <tewks@Carls-MacBook-Pro.local>
2016-12-11 11:17:49 -0800

    up to lesson 73

---
+++ BlockBreaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		print("Collision detected!");
+	}
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print("Trigger triggered!!");
+		levelManager.LoadLevel ("Start");	
+
+	}
+
+}
---
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+
+	void Awake () {
+		Debug.Log ("Music Player Awake" + GetInstanceID ());
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("The clone has been destroyed!");
+		}
+		else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+}
