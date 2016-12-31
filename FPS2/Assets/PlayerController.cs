9e7f860a6a52d0eb7436eebd008f60a1891690fe
Carl Tewksbury <packetbuddha@gmail.com>
2016-12-30 20:13:21 -0800

    fps2 working basic player controller

--- FPS2/Assets/PlayerController.cs
+++ FPS2/Assets/PlayerController.cs
@@ -3,59 +3,76 @@
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour {
 
-	[SerializeField]
-	float speedMultiplier = 5.0f;
-	float lookSensitivity = 50.0f;
 
-	float xMov;
-	float zMov;
-	float xRot;
-	float yRot;
 
-	PlayerMotor motor;
+	private float xMov;
+	private float zMov;
+	private float xRot;
+	private float yRot;
 
-	Vector3 playerVelocity = Vector3.zero;
-	Vector3 playerRotation = Vector3.zero;
-	Vector3 moveHorizontal = Vector3.zero;
-	Vector3 moveVertical = Vector3.zero;
-	 
-	//Vector3 cameraRotationX = Vector3.zero;
+	private PlayerMotor motor;
 
-	private void Start ()
+    private Vector3 playerVelocity;
+    private Vector3 playerRotation;
+    private Vector3 positionHorizontal;
+    private Vector3 positionVertical;
+    private Vector3 cameraRotationX;
+
+    private void Start ()
 	{
 		motor = GetComponent<PlayerMotor> ();
+
+        // Lock the mouse pointer so it doesn't interfere with player movment
+        Cursor.lockState = CursorLockMode.Locked;
 	}
 
 	private void Update ()
 	{
+        // Raw provides inputs which are always [-1, 0 or 1]
 		xMov = Input.GetAxisRaw ("Horizontal");
 		zMov = Input.GetAxisRaw ("Vertical");
 
-		Debug.Log("xMov: " + xMov + "; zMov: " + zMov);
-		Debug.Log("transform.right: " + transform.right);
-		Debug.Log("transofrm.forward: " + transform.forward);
+        //Debug.Log("xMov: " + xMov + "; zMov: " + zMov);
+        //Debug.Log("transform.right: " + transform.right);
+        //Debug.Log("transofrm.forward: " + transform.forward);
 
-		// transform.right is the horizontal value, relative to the object (not world space)
-		moveHorizontal = transform.right * xMov;
-		moveVertical = transform.forward * zMov;
+        // transfor.right and transform.forward are the world space vectors, relative to the players rotation
+        // So, as the player rotates, 'forward' and 'right' become relative values. We use these to construct
+        // our final player velocity vector.
+        // For example:
+        // forward start = (0, 0, 1)
+        // right start = (1, 0, 0)
+        // as we rotate to the right (clockwise), about 45 degress, these values become relative...
+        // our new forward = (.7, 0, .7)
+        // our new right = = (.7, 0, -.7)
+        positionHorizontal = transform.right * xMov;
+        positionVertical = transform.forward * zMov;
 
-		playerVelocity = (moveHorizontal + moveVertical).normalized * speedMultiplier;
+        // Create a velocity vector. Normalized ensures the magnitude is always 1.
+        // Magnitude is computed as sqrt(x*x + y*y + z*z). Magnitude is out speed reference.
+        // Without normalized, movement at a diagonal (x=1, y=1) has a magnitude of 1.414.
+        // So, in this case, it's keeping speed consistent.
+        playerVelocity = (positionHorizontal + positionVertical).normalized;
 
-		yRot = Input.GetAxisRaw ("Mouse X") * lookSensitivity;
-		//xRot += Input.GetAxisRaw ("Mouse Y") * lookSensitivity;
+        // Camera Rotation
 
-		// Calculate move vector
+        // Mouse movement will be a value between -1...1
+		yRot = Input.GetAxisRaw ("Mouse X");
+        xRot = Input.GetAxisRaw ("Mouse Y");
+
+        Debug.Log("xRot: " + xRot + "yRot: " + yRot);
+     
 		playerRotation = new Vector3(0f, yRot, 0f);
-		//cameraRotationX = new Vector3 (-xRot, 0.0f, 0.0f);
-
+		cameraRotationX = new Vector3 (-xRot, 0.0f, 0.0f);
 
 		motor.Move (playerVelocity);
-		
 		motor.Rotate (playerRotation);
+        motor.CameraRotate(cameraRotationX);
 
-		//if (cameraRotationX != Vector3.zero) {
-		//	motor.CameraRotate (cameraRotationX);
-		//}
+        // Relase the mouse pointer by hitting escape
+        if (Input.GetKeyDown("escape")) {
+            Cursor.lockState = CursorLockMode.None;
+        }
 
 	}
 }
--- FPS2/Assets/PlayerMotor.cs
+++ FPS2/Assets/PlayerMotor.cs
@@ -3,53 +3,65 @@
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour {
 
-	[SerializeField]
-	private Camera cam;
-	private Rigidbody rb;
+	[SerializeField] private Camera cam;
+    [SerializeField] float speedMultiplier = 25.0f;
+    [SerializeField] float lookSensitivity = 60.0f;
 
-	private Vector3 playerVelocity;
-	private Vector3 playerRotation;
+    private Rigidbody rb;
+	private Vector3 playerVelocity = Vector3.zero;
+	private Vector3 playerRotation = Vector3.zero;
+    private Vector3 cameraRotationX = Vector3.zero;
 
 
-	private void Start ()
+    private void Start ()
 	{
 		rb = GetComponent<Rigidbody> ();
 	}
 
 	public void  Move (Vector3 _playerVelocity)
 	{
-		playerVelocity = _playerVelocity * Time.fixedDeltaTime;
+		playerVelocity = _playerVelocity * speedMultiplier * Time.fixedDeltaTime;
 	}
 
 	public void Rotate (Vector3 _playerRotation)
 	{
-		playerRotation = _playerRotation * Time.fixedDeltaTime;
+		playerRotation = _playerRotation * lookSensitivity * Time.fixedDeltaTime;
 	}
 
 	public void CameraRotate (Vector3 _cameraRotationX)
 	{
-		cam.transform.eulerAngles = _cameraRotationX;
-	}
+		cameraRotationX = _cameraRotationX * lookSensitivity * Time.fixedDeltaTime;
+    }
 
 	private void PeformPlayerMove ()
 	{
-		rb.MovePosition (rb.position + playerVelocity);
+        //Debug.Log(playerVelocity.x + "," + playerVelocity.z + ", M: " + playerVelocity.magnitude);
+        rb.position += playerVelocity;
 	}
 
 	private void PerformPlayerRotate ()
 	{
-		rb.MoveRotation (rb.rotation * Quaternion.Euler(playerRotation));
+        rb.rotation = (rb.rotation * Quaternion.Euler(playerRotation));
 	}
 
 	private void PerformCameraRotate ()
 	{
-		
+        cam.transform.rotation = (cam.transform.rotation * Quaternion.Euler(cameraRotationX));
 	}
 
 	private void FixedUpdate ()
 	{
-		PeformPlayerMove();
-		PerformPlayerRotate();
-	}
-	
+        if (playerVelocity != Vector3.zero)
+        {
+            PeformPlayerMove();
+        }
+        if (playerRotation != Vector3.zero)
+        {
+            PerformPlayerRotate();
+        }
+        if (cameraRotationX != Vector3.zero)
+        {
+            PerformCameraRotate();
+        }     
+	}	
 }
